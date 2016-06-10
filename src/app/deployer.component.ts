ec995c4692a2a24598d88298099a1b71a1c32cf4
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-10 12:37:43 -0400

    added login bare component

--- src/app/deployer.component.ts
+++ src/app/deployer.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './+login';
+import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'deployer-app',
   templateUrl: 'deployer.component.html',
-  styleUrls: ['deployer.component.css']
+  styleUrls: ['deployer.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/login', component: LoginComponent}
+])
 export class DeployerAppComponent {
   title = 'deployer works!';
 }
