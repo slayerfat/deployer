c2f72ef4aace8af1dde039ea0d57665c33c30ad8
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-10 17:04:26 -0400

    added dashboard bare component

--- src/app/deployer.component.ts
+++ src/app/deployer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './+login';
 import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { DashboardComponent } from './+dashboard';
 
 @Component({
   moduleId: module.id,
@@ -11,7 +12,8 @@ import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
   providers: [ROUTER_PROVIDERS]
 })
 @Routes([
-  {path: '/login', component: LoginComponent}
+  {path: '/login', component: LoginComponent},
+  {path: '/dashboard', component: DashboardComponent}
 ])
 export class DeployerAppComponent {
   title = 'deployer works!';
