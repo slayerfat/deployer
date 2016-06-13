98df2c0fc7ca5e4b041fe509bb29d736d7548e60
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-12 22:13:34 -0400

    added mongoose database connection

---
+++ server/database.ts
@@ -0,0 +1,42 @@
+import config from '../config/config';
+
+export default (mongoose) => {
+  let url;
+
+  if (config.env == 'development') {
+    url = config.mongo.development;
+  } else {
+    url = config.mongo.production;
+  }
+
+  mongoose.connect(url, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+
+  mongoose.connection.on('connected', () => {
+    console.log(`Connected to ${config.env} database.`);
+  });
+
+  // If the connection throws an error
+  mongoose.connection.on('error', (err) => {
+    console.error(`Unable to connect to ${config.env} database`, err);
+  });
+
+  // When the connection is disconnected
+  mongoose.connection.on('disconnected', () => {
+    console.log(`Disconnected from ${config.env} database.`);
+  });
+
+  let exit = function() {
+    mongoose.connection.close(() => {
+      console.log(`Database ${config.env} disconnected thanks to node.`);
+
+      process.exit(0);
+    });
+  };
+
+  // If the Node process ends, close the Mongoose connection
+  process.on('SIGINT', exit).on('SIGTERM', exit);
+};
--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,28 @@ import { Request, Response } from 'express';
 import express = require('express');
 import path = require('path');
 import loginRoute from './routes/auth/login.route';
+import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
+import config from '../config/config';
+import * as mongoose from 'mongoose';
+import db from './database';
 
 const rootDir = path.resolve('dist/');
 const indexFile = path.resolve('dist/index.html');
 
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static(rootDir));
+
+db(mongoose);
+
+if (config.env == 'development') {
+  // use morgan to log requests to the console
+  // noinspection TypeScriptValidateTypes
+  app.use(morgan('dev'));
+}
 
 app.get('/*', function (req: Request, res: Response) {
   res.sendFile(indexFile);
