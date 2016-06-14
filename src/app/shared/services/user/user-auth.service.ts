7c10233ac859b71fafdfd56e20cf0660fb3c4aaa
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-13 23:51:29 -0400

    restructured shared folder according to structure

--- src/app/shared/services/user/user-auth.service.ts
+++ src/app/shared/services/user/user-auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import { LoginResponse } from '../interfaces/server/LoginResponse';
+import { LoginResponse } from '../../interfaces/server/LoginResponse';
 import 'rxjs/add/operator/map';
-import { environment } from '../../environment';
+import { environment } from '../../../environment';
 
 @Injectable()
 export class UserAuthService {
