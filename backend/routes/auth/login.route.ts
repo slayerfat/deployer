447a6c5e0c94bde4eda66eda6bc74960c521aa72
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-17 14:36:12 -0400

    updated auth login with promises

--- backend/routes/auth/login.route.ts
+++ backend/routes/auth/login.route.ts
@@ -2,65 +2,52 @@ import { Request, Response } from 'express';
 import User from '../../models/users/User';
 import * as jwt from 'jsonwebtoken';
 import { Comprobable } from '../../../src/app/shared/interfaces';
+import { UserModelInterface } from '../../models/users/UserModelInterface';
 
 export default function loginRoute(app) {
   app.post('/api/login', function (req: Request, res: Response) {
-    User.findOne({name: req.body.name}, function (err, user) {
-      if (err) {
-        return console.log(err);
+    const message = {success: false, message: 'Authentication failed.'};
+
+    User.findOne({name: req.body.name}).exec().then((user: UserModelInterface) => {
+      if (!user || user.password != req.body.password) {
+        return Promise.resolve(res.json(message));
       }
 
-      const message = {success: false, message: 'Authentication failed.'};
+      // creates the jwt toke with a 6 hours duration
+      // since jwt typing states sign returns void, we have
+      // to assert the return as any (string doesn't work)
+      let token = jwt.sign(user, app.get('jwtSecret'), {
+        expiresIn: '6h'
+      }) as any;
 
-      if (!user) {
-        return res.json(message);
-      } else if (user) {
-        // check if password matches
-        if (user.password != req.body.password) {
-          res.json(message);
-        } else {
-          // creates the jwt toke with a 6 hours duration
-          // since jwt typing states sign returns void, we have
-          // to assert the return as any (string doesn't work)
-          let token = jwt.sign(user, app.get('jwtSecret'), {
-            expiresIn: '6h'
-          }) as any;
+      let response: Comprobable = {success: true, token: token};
 
-          let response: Comprobable = {success: true, token: token};
+      // returns the token with a success message
+      return Promise.resolve(res.json(response));
+    }, err => {
+      console.log(err);
 
-          // returns the token with a success message
-          res.json(response);
-        }
-      }
+      return Promise.resolve(res.status(400).json(message));
     });
   });
 
-  app.post('/api/seed', function (req, res) {
+  app.get('/api/seed', function (req: Request, res: Response) {
     // create a sample user
-    let user = new User({
+    let user: UserModelInterface = new User({
       name: 'asd',
       password: 'asd',
-      email: 'a@a',
-      control: {
-        dates: {
-          created_at: null,
-          updated_at: null
-        },
-        user: {
-          created_by: 1,
-          updated_by: 1
-        }
-      }
+      email: 'a@a'
     });
 
     // save the sample user
-    user.save(function (err) {
-      if (err) {
-        console.log(err);
-      }
-
+    user.save().then(() => {
       console.log('User saved successfully.');
+
       res.json({success: true});
+    }, err => {
+      console.log(err);
+
+      res.status(400).json({success: false, err: err});
     });
   });
 }
