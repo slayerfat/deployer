298061e3bc55786500db41652810897b142d7e7b
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-17 22:48:27 -0400

    updated indent

--- backend/models/shared/appendUsers.ts
+++ backend/models/shared/appendUsers.ts
@@ -2,23 +2,21 @@ import User from '../users/User';
 
 export default function appendUsers(next) {
   // TODO: get current user
-  User.findOne()
-    .exec()
-    .then(model => {
-      // we need to check if the model is not null (first user)
-      const id = model ? model._id : 0;
+  User.findOne().exec().then(model => {
+    // we need to check if the model is not null (first user)
+    const id = model ? model._id : 0;
 
-      if (!this.createdBy) {
-        this.createdBy = id;
-      }
+    if (!this.createdBy) {
+      this.createdBy = id;
+    }
 
-      this.updatedBy = id;
+    this.updatedBy = id;
 
-      return next();
-    }, err => {
-      console.log('error appending user control.');
-      console.log(err);
+    return next();
+  }, err => {
+    console.log('error appending user control.');
+    console.log(err);
 
-      return next(new Error(err));
-    });
+    return next(new Error(err));
+  });
 }
