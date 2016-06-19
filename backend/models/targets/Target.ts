3bd01fbdeb8de72f2c0733d013b3d05d726cc077
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-19 12:01:26 -0400

    removed schemaPartial idea for now

--- backend/models/targets/Target.ts
+++ backend/models/targets/Target.ts
@@ -2,25 +2,42 @@ import * as mongoose from 'mongoose';
 import { TargetModelInterface } from './TargetModelInterface';
 import appendDates from '../shared/appendDates';
 import appendControl from '../shared/appendUsers';
-import options from '../shared/controlSchemaPartial';
 
-options['user'] = {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'User',
-  required: true
-};
-
-options['name'] = {
-  type: String,
-  required: true
-};
-
-options['commands'] = {
-  type: String,
-  required: true
-};
-
-let targetSchema: mongoose.Schema = new mongoose.Schema(options);
+let targetSchema: mongoose.Schema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    max: 100,
+    match: /^([\w ,.!?]{1,100})$/
+  },
+  commands: {
+    type: String,
+    required: true,
+    trim: true,
+    max: 1000
+  },
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
 
 targetSchema.pre('save', appendDates);
 targetSchema.pre('save', appendControl);
--- backend/models/users/User.ts
+++ backend/models/users/User.ts
@@ -2,30 +2,42 @@ import * as mongoose from 'mongoose';
 import { UserModelInterface } from './UserModelInterface';
 import appendDates from '../shared/appendDates';
 import appendControl from '../shared/appendUsers';
-import options from '../shared/controlSchemaPartial';
 
-options['name'] = {
-  type: String,
-  required: true,
-  unique: true
-};
-
-options['email'] = {
-  type: String,
-  required: true,
-  unique: true
-};
-
-options['password'] = {
-  type: String,
-  required: true
-};
-
-options['remember_token'] = {
-  type: String
-};
-
-let userSchema: mongoose.Schema = new mongoose.Schema(options);
+let userSchema: mongoose.Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    max: 25,
+    min: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  remember_token: String,
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
 
 userSchema.pre('save', appendDates);
 userSchema.pre('save', appendControl);
