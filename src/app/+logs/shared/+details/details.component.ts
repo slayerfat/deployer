ff85b958ed2b61457e7eb3b36d17484571e10760
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-28 13:12:51 -0400

    cleaned view a bit inside logs

--- src/app/+logs/shared/+details/details.component.ts
+++ src/app/+logs/shared/+details/details.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-// import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LogService, LogInterface } from '../../../shared';
 
 @Component({
   moduleId: module.id,
   templateUrl: 'details.component.html',
   styleUrls: ['details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  public log: LogInterface;
+  private sub: any;
+
+  constructor(private route: ActivatedRoute, private logService: LogService) {
   }
 
+  // noinspection JSUnusedGlobalSymbols
   public ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.logService.getOne(params['id']).subscribe(res => this.log = res);
+    });
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  public ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
