9665678610f0fc3784e2d25261e18f767338053b
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-28 13:11:48 -0400

    log service gets a single log

--- src/app/shared/services/log/log.service.ts
+++ src/app/shared/services/log/log.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import { environment } from '../../../environment';
 import { Gettable } from '../interfaces/Gettable';
 import { BackendHttpService } from '../misc/backend-http.service';
+import { LogInterface } from '../../../shared';
 
 @Injectable()
-export class LogService extends BackendHttpService implements Gettable {
+export class LogService extends BackendHttpService implements Gettable<LogInterface> {
 
   constructor(http: Http) {
     super(http);
   }
 
-  public getAll(): Observable<Response> {
+  public getAll(): Observable<LogInterface[]> {
     return this.http.get(environment.endpoints.logs)
       .map((res) => res.json())
       .catch(this.handleError);
   }
 
-  public getOne(id: number): Observable<Response> {
-    return this.http.get(`${environment.endpoints.logs}/${id}`)
-      .map((res) => {
-        console.log(res);
-        res.json();
-      }).catch(this.handleError);
+  public getOne(id: number): Observable<LogInterface> {
+    return this.http.post(
+      `${environment.endpoints.logs}/find`,
+      {id: id},
+      {headers: this.headers}
+    ).map((res) => res.json()).catch(this.handleError);
   }
 }
