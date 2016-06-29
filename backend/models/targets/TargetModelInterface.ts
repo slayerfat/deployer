171e1f6f9933a675799fd4001c93084daae12f8b
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-29 16:50:11 -0400

    fixed target interface name bug

--- backend/models/targets/TargetModelInterface.ts
+++ backend/models/targets/TargetModelInterface.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 import { ControlInterface } from '../shared/interfaces/ControlInterface';
 import { Sluggable } from '../shared/interfaces/Sluggable';
-import { TargetInterface } from './TargetlInterface';
+import { TargetInterface } from './TargetInterface';
 
 export interface TargetModelInterface extends mongoose.Document,
   TargetInterface,
