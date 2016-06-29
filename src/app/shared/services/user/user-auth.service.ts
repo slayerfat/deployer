f22dbb3b99e893878c12247979b7441790e18afb
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-29 13:59:56 -0400

    removed unused headers

--- src/app/shared/services/user/user-auth.service.ts
+++ src/app/shared/services/user/user-auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { LoginResponse } from '../../interfaces/server/LoginResponse';
 import 'rxjs/add/operator/map';
