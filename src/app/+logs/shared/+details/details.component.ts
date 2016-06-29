336feb7e19817af531d9684ca7ce6c6842dafecf
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-29 13:39:40 -0400

    updated log view

--- src/app/+logs/shared/+details/details.component.ts
+++ src/app/+logs/shared/+details/details.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { LogService, LogInterface } from '../../../shared';
+import { LogService, LogInterface, HumanDiff } from '../../../shared';
+import { TAB_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
   templateUrl: 'details.component.html',
-  styleUrls: ['details.component.css']
+  styleUrls: ['details.component.css'],
+  directives: [TAB_DIRECTIVES],
+  pipes: [HumanDiff]
 })
 export class DetailsComponent implements OnInit, OnDestroy {
 
@@ -25,5 +28,16 @@ export class DetailsComponent implements OnInit, OnDestroy {
   // noinspection JSUnusedGlobalSymbols
   public ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  public setStatusIconClasses(status?: any) {
+    status = status || this.log.status;
+
+    return {
+      'fa-times-circle-o': !status,
+      'text-danger': !status,
+      'fa-check-circle-o': status,
+      'text-success': status,
+    };
   }
 }
