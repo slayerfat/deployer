119ee780893c121a1311830cea04da7a478edf19
Alejandro Granadillo <slayerfat@gmail.com>
2016-07-12 16:47:04 -0400

    added logs to dashboard

--- src/app/+dashboard/dashboard.component.ts
+++ src/app/+dashboard/dashboard.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LogService } from '../shared/services/log/log.service';
+import { LogInterface } from '../shared/interfaces/models/LogInterface';
+import { HumanDiff } from '../shared/pipes/human-diff.pipe';
 
 @Component({
   moduleId: module.id,
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html',
-  styleUrls: ['dashboard.component.css']
+  styleUrls: ['dashboard.component.css'],
+  providers: [LogService],
+  pipes: [HumanDiff]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-  constructor() {}
+  private logs: LogInterface[];
+
+  constructor(private logsService: LogService) {
+  }
+
+  public ngOnInit(): any {
+    return this.logsService.getLatest().subscribe(logs => {
+      this.logs = logs;
+    });
+  }
+
+  public setStatusClasses(status) {
+    return {
+      'fa-times-circle-o': !status,
+      'text-danger': !status,
+      'fa-check-circle-o': status,
+      'text-success': status,
+    };
+  }
 }
